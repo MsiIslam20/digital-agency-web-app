0906f70e9c7f7d601de094b77c7a549da037c577
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-12 23:38:48 +0600

    header done

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import './Header.css';
+import home from '../../../images/home.png'
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="home-wrapper">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12 px-0">
+                        <div className="row">
+                            <div className="col-md-5">
+                                <div className="home-inner">
+                                    <h1>Let’s Grow Your Brand To The Next Level</h1>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus commodo ipsum duis laoreet maecenas. Feugiat </p>
+                                    <button className="btn btn-primary btn-brand">Hire us</button>
+                                </div>
+                            </div>
+                            <div className="col-md-7">
+                                <img src={home} alt="" className="img-fluid"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
