90ec24cab45a08161a77e1873c5822cacf98deb0
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-13 10:35:01 +0600

    carousel added

--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -3,6 +3,7 @@ import Client from '../Client/Client';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import Navbar from '../Navbar/Navbar';
+import OurWorks from '../OurWorks/OurWorks';
 import Review from '../Review/Review';
 import Services from '../Services/Services';
 
@@ -13,6 +14,7 @@ const HeaderMain = () => {
             <Header />
             <Client />
             <Services />
+            <OurWorks />
             <Review />
             <Footer />
         </>
---
+++ src/components/Home/OurWorks/OurWorks.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './OurWorks.css'
+
+const OurWorks = () => {
+    return (
+        <section className="works-wrapper section-padding">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1 className="title text-white">Here are some of <span>our works</span></h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default OurWorks;
