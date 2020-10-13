69da70e01896405491403b0b445058f0548865b6
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-13 10:57:31 +0600

    loogin page added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
+import Login from './components/Login/Login';
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
         <Route exact path="/">
           <HeaderMain />
         </Route>
+        <Route path="/login">
+          <Login />
+        </Route>        
       </Switch>
     </Router>
   );
---
+++ src/components/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <section className="login-wrapper">
+            
+        </section>
+    );
+};
+
+export default Login;
