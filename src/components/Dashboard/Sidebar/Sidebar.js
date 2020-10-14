19770ffa3b1a4fae33188c95808bdf8e715c32b8
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-14 13:20:54 +0600

    admin access done

--- src/components/Dashboard/Sidebar/Sidebar.js
+++ src/components/Dashboard/Sidebar/Sidebar.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../../App';
 import logo from '../../../images/logo.svg';
 
 const Sidebar = () => {
+
+    const [loggedInUser , setLoggedInUser] = useContext(UserContext);
+    const [isDoctor, setIsDoctor] = useState(false);
+
+    useEffect(() => {
+        fetch('http://localhost:4000/isAdmin', {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({ email: loggedInUser.email })
+        })
+            .then(res => res.json())
+            .then(data => setIsDoctor(data));
+    }, []);
+
     return (
         <div className="col-md-3 pr-0">
             <div className="order-sidebar">
@@ -15,21 +30,26 @@ const Sidebar = () => {
                     {/* <li>
                         <Link to="/orders">Order</Link>
                     </li> */}
-                    <li>
-                        <Link to="/serviceList">Service list</Link>
-                    </li>
-                    <li>
-                        <Link to="/reviews">Review</Link>
-                    </li>
-                    <li>
-                        <Link to="/allService">Service list</Link>
-                    </li>
-                    <li>
-                        <Link to="/addService">Add Service</Link>
-                    </li> 
-                    <li>
-                        <Link to="/makeAdmin">Make Admin</Link>
-                    </li>                                         
+                    {
+                        isDoctor ?<div>
+                            <li>
+                                <Link to="/allService">Service list</Link>
+                            </li>
+                            <li>
+                                <Link to="/addService">Add Service</Link>
+                            </li> 
+                            <li>
+                                <Link to="/makeAdmin">Make Admin</Link>
+                            </li>
+                        </div> : <div>
+                            <li>
+                                <Link to="/serviceList">Service list</Link>
+                            </li>
+                            <li>
+                                <Link to="/reviews">Review</Link>
+                            </li>
+                        </div>
+                    }
                 </ul>
             </div>
         </div>
