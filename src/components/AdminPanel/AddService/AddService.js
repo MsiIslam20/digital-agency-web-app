a818c4264d73a5f673a995674e2babb513c10aba
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-15 11:34:34 +0600

    final release

--- src/components/AdminPanel/AddService/AddService.js
+++ src/components/AdminPanel/AddService/AddService.js
@@ -33,7 +33,7 @@ const AddService = () => {
         })
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+            alert("service added successfully...!!!!!")
         })
         .catch(error => {
             console.error(error)
